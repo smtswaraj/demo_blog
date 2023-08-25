4240b608ca3bf992fb6e6d6bbbfb4b7565e7a61a
Swaraj <swarajnayakdipu227@gmail.com>
2023-08-25 07:04:08 +0530

    ojkjkjkj

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = 'static/'
 import os
 
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
